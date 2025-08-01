9af1e00e69f1fe1bb03a87f1a04acd63ceef93c0
Manuel Esteban Erazo Medina <meerazo7@hotmail.com>
2025-08-01 11:24:27 -0500

    feat: SEO home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
 import HomePageClient from '@/components/landing/HomePageClient';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'eduTrack: Gestión de Asistencia con QR para la FUP',
-  description:
-    'Bienvenido a eduTrack, la solución moderna para la gestión de asistencia en la FUP. Simplifica el seguimiento de estudiantes con códigos QR, ahorra tiempo y mejora la eficiencia académica.',
-};
 
 export default function HomePage() {
   return <HomePageClient />;
